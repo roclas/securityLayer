d5346d9c7e7d33acc280f85409c907447a83e5b5
carlos <carlopolo@gmail.com>
2014-03-16 20:25:18 +0100

    Update proxy_app.js
    post and put requests weren't correctly proxied

--- proxy_app.js
+++ proxy_app.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
-var users = [
-  {id:0, username: 'carlos', password:'password'}
-];
+var users = [ {id:0, username: 'carlos', password:'password'} ];
 
 var passport = require("passport"), express=require("express"),LocalStrategy = require('passport-local').Strategy,fs=require('fs'), jade = require('jade'), httpProxy=require('http-proxy'), bhost="localhost", bport=9001;
+var rest = require('restler');
+//var _=require('underscore');
 
-var proxy = new httpProxy.RoutingProxy();
+//var proxy = new httpProxy.RoutingProxy();
+var proxy = httpProxy.createProxyServer({target: { host: 'localhost', port: 9001 } });
 
 var app = express();
 
@@ -50,21 +51,20 @@ app.get('/logout',  function(req, res){
 app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }));
 app.get('/login', function(req, res) { res.sendfile('./login.html'); });
 
-app.get(/^\/.?/, function(req, res) {
-  //if(req.isAuthenticated()) {
+
+
+app.all(/^\/.?/, function(req, res) {
   if(req.session.passport.user) {
-   console.log('user ', req.session.passport.user, " logged in");
+   //console.log('user ', req.session.passport.user, " logged in");
    req.headers['iv-user']=req.session.passport.user;
    res.setHeader("iv-user",req.headers["iv-user"]);
-   //console.log(req.params);
-   try{
-        proxy.proxyRequest(req, res ,{
-                host: bhost,
-                port: bport
-        });
-   }catch(error){
-        res.status(404).sendfile('notfound.html');
-   }
+   if(req.method=="PUT") { 
+	//req.params=_.extend(req.params || {}, req.query || {}, req.body || {});
+	rest.put('http://'+bhost+':'+bport+req.url, req).on('complete', function(data, response) { res.end(data); });
+  }else  if(req.method=="POST") { 
+	rest.post('http://localhost:9001'+req.url, req).on('complete', function(data, response) { res.end(data); });
+  }else 
+   	proxy.web(req, res);
   }else {
    req.session.attempting=req.url;
    console.log('user not logged in '+JSON.stringify(req.session.passport));
