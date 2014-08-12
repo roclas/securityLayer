3ce930a24cbc8601a13c4d7062f1fda0cf34c84e
Carlos <carlopolo@gmail.com>
2014-08-12 16:11:48 +0200

    it is much simpler now and works with put and post requests

--- proxy_app.js
+++ proxy_app.js
@@ -1,80 +1,60 @@
-#!/usr/bin/env node
-
-var users = [ {id:0, username: 'carlos', password:'password'} ];
-
-var passport = require("passport"), express=require("express"),LocalStrategy = require('passport-local').Strategy,fs=require('fs'), jade = require('jade'), httpProxy=require('http-proxy'), bhost="localhost", bport=9001;
-var rest = require('restler');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express');
+var http = require('http');
+var httpProxy = require('http-proxy');
 var expressSession= require('express-session');
-//var _=require('underscore');
+var qs = require('querystring');
 
-//var proxy = new httpProxy.RoutingProxy();
-var proxy = httpProxy.createProxyServer({target: { host: bhost, port: 8000} });
 
+var proxy = httpProxy.createProxyServer({target:'http://localhost:80'});
 var app = express();
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id+"");
-});
-
-passport.deserializeUser(function(id, done) {
-  done(null, users[id]);
-});
-
-passport.use(new LocalStrategy(function(username, password,done){
-        for(u in users){
-         if(users[u].username == username ){
-          if (password== users[u].password) return done(null, users[u]);
-          done(null, false, { message: 'Incorrect password.' });
-         }
-        }
-        return done(null, false, { message: 'Incorrect username.' });
-}));
-
-
-app.use(cookieParser());
-app.use(bodyParser());
 app.use(expressSession({ secret: 'SECRET' }));
-app.use(passport.initialize());
 
 
-app.get('/logout',  function(req, res){
-   if (req.session) {
-    req.session.auth = null;
-    res.clearCookie('auth');
-    req.session.destroy(function() {});
+
+//app.use(function(req, res, next) {
+var auth=function(req,res,next){
+  if (!req.session || !req.session.user_id) {
+    res.redirect('/login');
+    //res.send('You are not authorized to view this page');
+  } else {
+    next();
   }
-  res.header('Cache-Control', 'no-cache');
-  res.header('Expires', 'Fri, 31 Dec 1998 12:00:00 GMT');
-  res.redirect('/login');
-});
+}
 
-
-
-app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }));
 app.get('/login', function(req, res) { res.sendfile('./login.html'); });
+ 
+app.post('/login', function (req, res) {
+        var body = '';
+        req.on('data', function (data) {
+            body += data;
 
-
-app.all(/^\/.?/, function(req, res) {
-  if(req.session.passport.user) {
-   //console.log('user ', req.session.passport.user, " logged in");
-   req.headers['iv-user']=req.session.passport.user;
-   res.setHeader("iv-user",req.headers["iv-user"]);
-   if(req.method=="PUT") { 
-	//req.params=_.extend(req.params || {}, req.query || {}, req.body || {});
-	rest.put('http://'+bhost+':'+bport+req.url, req).on('complete', function(data, response) { res.end(data); });
-  }else  if(req.method=="POST") { 
-	rest.post('http://'+bhost+':'+bport+req.url, req).on('complete', function(data, response) { res.end(data); });
-  }else 
-   	proxy.web(req, res);
-  }else {
-   req.session.attempting=req.url;
-   console.log('user not logged in '+JSON.stringify(req.session.passport));
-   res.sendfile('./login.html');
-  }
+            // Too much POST data, kill the connection!
+            if (body.length > 1e6)
+                req.connection.destroy();
+        });
+        req.on('end', function () {
+            var post = qs.parse(body);
+  	    if (post.username === 'carlos' && post.password === 'password') {
+		if(!req.session)req.session={};
+            	req.session.user_id = post.username;
+            	res.redirect('/form.html');
+  	    } else {
+    		//res.redirect('/login');
+    		res.send('Bad user/pass');
+  	    }
+        });
 });
 
-app.use(passport.session());
+app.get('/logout', auth,function (req, res) {
+  delete req.session.user_id;
+  res.redirect('/login');
+}); 
 
-app.listen(8801);
+
+//app.all(/^\/.?/,auth, function(req, res) {
+app.all(/^\/.?/, auth,function(req, res) {
+	proxy.web(req, res);
+});
+
+
+app.listen(process.env.PORT || 8001);
